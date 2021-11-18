4e558cc8e2f3805e05ddda71d8b1ea1322376dd7
Lisa Clifford <ljoyclifford@gmail.com>
2021-11-18 14:47:07 -0500

    Modify html, creat api key and query selector

---
+++ index.js
@@ -0,0 +1,4 @@
+let searchInputDom = document.querySelector("search-input");
+let apiKey = "2732988f21c44febb520f8872d9bddaa";
+let favoriteList = [];
+let favoriteAmountDom = document.querySelector("#favoriteAmount");
