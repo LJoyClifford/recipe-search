700a620ac3ae080d839758a1cd473d7296992323
Lisa Clifford <ljoyclifford@gmail.com>
2021-11-29 12:30:52 -0500

    fix bugs

--- index.js
+++ index.js
@@ -4,101 +4,100 @@ let favoriteList = [];
 let favoriteAmountDom = document.querySelector("#favoriteAmount");
 
 searchInputDom.addEventListener("keypress", function (e) {
-    let ingredients = e.target.value;
-   
-    if (e.key === "Enter") {
-      searchInputDom.value = "";
-      fetch(
-        `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${ingredients}&apiKey=${apiKey}`
-      )
-        .then((respond) => respond.json())
-        .then((list) => renderRecipes(list.results));
-    } 
-    console.log("you pressed", e.key);
-  });
+  let ingredients = e.target.value;
 
-  function renderRecipes(list) {
-    let recipesListDom = document.querySelector("#recipes-list");
+  if (e.key === "Enter") {
+    searchInputDom.value = "";
+    fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${ingredients}&apiKey=${apiKey}`
+    )
+      .then((respond) => respond.json())
+      .then((list) => renderRecipes(list.results));
+  }
+  console.log("you pressed", e.key);
+});
 
-    recipesListDom.innerHTML = "";
-    list.forEach((recipe) => {
-      let recipeContainer = document.createElement("div");
-      recipeContainer.classList.add("recipe");
+function renderRecipes(list) {
+  let recipesListDom = document.querySelector("#recipes-list");
 
-      let title = document.createElement("div");
-      title.classList.add("title");
+  recipesListDom.innerHTML = "";
+  list.forEach((recipe) => {
+    let recipeContainer = document.createElement("div");
+    recipeContainer.classList.add("recipe");
+
+    let title = document.createElement("div");
+    title.classList.add("title");
+    title.innerText = recipe.title;
+    recipeContainer.appendChild(title);
+
+    recipeContainer.addEventListener("mouseover", function () {
+      title.innerText = "Open this recipe";
+    });
+    recipeContainer.addEventListener("mouseleave", function () {
       title.innerText = recipe.title;
-      recipeContainer.appendChild(title);
+    });
 
-      recipeContainer.addEventListener("mouseover", function () {
-        title.innerText = "Open this recipe";
-      });
-      recipeContainer.addEventListener("mouseleave", function () {
-        title.innerText = recipe.title;
-      });
+    let recipeImg = document.createElement("img");
+    recipeImg.src = recipe.image;
+    recipeContainer.appendChild(recipeImg);
 
-      let recipeImg = document.createElement("img");
-      recipeImg.src = recipe.image;
-      recipeContainer.appendChild(recipeImg);
+    let likeBtn = document.createElement("button");
+    likeBtn.innerHTML = "&#10084;";
+    likeBtn.addEventListener("mouseover", function (e) {
+      e.stopPropagation();
+      if (recipeContainer.classList.contains("liked")) {
+        title.innerText = "Remove this recipe from favorites";
+      } else {
+        title.innerText = "Add this recipe to favorites";
+      }
+    });
+    likeBtn.addEventListener("mouseleave", function () {
+      title.innerText = recipe.title;
+    });
+    likeBtn.onclick = function (e) {
+      e.stopPropagation();
 
-      let likeBtn = document.createElement("button");
-      likeBtn.innerHTML = "&#10084;";
-      likeBtn.addEventListener("mouseover", function (e) {
-        e.stopPropagation();
-        if (recipeContainer.classList.contains("liked")) {
-          title.innerText = "Remove this recipe to favorites";
-        } else {
-          title.innerText = "Add this recipe to favorites";
-        }
-      });
-     likeBtn.addEventListener("mouseleave", function () {
-        title.innerText = recipe.title;
-      });
-      likeBtn.onclick = function (e) {
-        e.stopPropagation();
+      recipeContainer.classList.toggle("liked");
+      if (recipeContainer.classList.contains("liked")) {
+        favoriteList.push(recipe);
+        title.innerText = "Remove this recipe from favorites";
+      } else {
+        let recipeIndexToRemove = favoriteList.findIndex(function (rec) {
+          return rec.id == recipe.id;
+        });
+        favoriteList.splice(recipeIndexToRemove, 1);
+        title.innerText = "Add this recipe to favorites";
+      }
+      favoriteAmountDom.innerText = favoriteList.length;
+    };
+    likeBtn.classList.add("likeBtn");
+    recipeContainer.appendChild(likeBtn);
 
-        recipeContainer.classList.toggle("liked");
-        if (recipeContainer.classList.contains("liked")) {
-          favoriteList.push(recipe);
-          title.innerText = "Remove this recipe from favorites";
-        } else {
-          let recipeIndexToRemove = favoriteList.findIndex(function (rec) {
-            return rec.id == recipe.id;
-          });
-          favoriteList.splice(recipeIndexToRemove, 1);
-          title.innerText = "Add this recipe to favorites";
-        }
-        favoriteAmountDom.innerText = favoriteList.length;
-      };
-      likeBtn.classList.add("likeBtn");
-      recipeContainer.appendChild(likeBtn);
+    let closeBtn = document.createElement("button");
+    closeBtn.classList.add("close");
+    closeBtn.innerText = "x";
+    recipeContainer.appendChild(closeBtn);
 
-      let closeBtn = document.createElement("button");
-      closeBtn.classList.add("close");
-      closeBtn.innerText = "x";
-      recipeContainer.appendChild(closeBtn);  
-
-      recipesListDom.appendChild(recipeContainer);
+    recipesListDom.appendChild(recipeContainer);
 
     title.addEventListener("click", function () {
       fetch(
         `https://api.spoonacular.com/recipes/${recipe.id}/information?apiKey=${apiKey}`
       )
-      
-      .then((respond) => respond.json())
-      .then((recipe) => {
-        let instructionsDom = document.createElement("div");
-        instructionsDom.classList.add("instructions");
-        instructionsDom.innerHTML = recipe.instructions;
-        recipeContainer.appendChild(instructionsDom);
-        recipeContainer.classList.add("selected");
+        .then((respond) => respond.json())
+        .then((recipe) => {
+          let instructionsDom = document.createElement("div");
+          instructionsDom.classList.add("instructions");
+          instructionsDom.innerHTML = recipe.instructions;
+          recipeContainer.appendChild(instructionsDom);
+          recipeContainer.classList.add("selected");
 
-        closeBtn.onclick = function (e) {
+          closeBtn.onclick = function (e) {
             e.stopPropagation();
             recipeContainer.classList.remove("selected");
-            recipeContainer.removeChild(summaryDom);
+            recipeContainer.removeChild(instructionsDom);
           };
         });
     });
   });
-  }
+}
